7a1f821d92c2b0e729fcb43e6b2a695ebac25418
nitesh <niteshmuz06@gmail.com>
2025-05-18 13:58:59 +0530

    understand data type with practical

---
+++ datatypes.exs
@@ -0,0 +1,31 @@
+#integers:
+
+integer1 = 34
+integer2 = -255489715348957349858952347985
+integer3 = 5834754389482390573489512390587851389085971593
+
+
+sum  = integer1 + integer2
+product = integer2 * integer3
+
+IO.puts("Integer1 : #{integer1}")
+IO.puts("Integer2 : #{integer2}")
+IO.puts("Integers3 : #{integer3}")
+IO.puts("Sum : #{sum}")
+IO.puts("product : #{product}")
+
+
+#float numbers
+
+float1 =43.4
+float2 = -34.5
+float3 = 1.0e-10
+
+sub= float1 - float2
+divide = float1 / float3
+
+IO.puts("float1 : #{float1}")
+IO.puts("float2 : #{float2}")
+IO.puts("float2 : #{float3}")
+IO.puts("substract : #{sub}")
+IO.puts("divide : #{divide}")
